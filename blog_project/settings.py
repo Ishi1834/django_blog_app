783a4506bd2c18f6ef63db5dc0ce17d680001c79
Ismail Mohamad <ikmohamad19@gmail.com>
2022-02-14 16:47:10 +0000

    chnaged static files storage

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+#line 125 changed to line 126 to see if server error is resolved
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
